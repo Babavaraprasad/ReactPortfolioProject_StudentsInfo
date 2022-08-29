3003252bb7a449cb930f1e7b09bab3c865b80981
Babavaraprasad <bapi@itu.dk>
2022-08-29 15:30:06 +0200

    added new component card to style each datalist

--- student/src/components/ProfileInfo/InfoRecord.js
+++ student/src/components/ProfileInfo/InfoRecord.js
@@ -1,9 +1,10 @@
+import Card from "../UI/Card";
 import classes from "./InfoRecord.module.css";
 
 function InfoRecord(props) {
   return (
         <li className={classes.item}>
-           
+           <Card>
           <div className={classes.image}>
               <img src={props.image} alt="Image not found" />
           </div>
@@ -23,6 +24,7 @@ function InfoRecord(props) {
           </div>
           <br/>
           <br/>
+          </Card>
       </li>
   );
 }
