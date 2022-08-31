bb26bcf0185b2e22bd8cd1237c3264c7744bd8f1
Babavaraprasad <bapi@itu.dk>
2022-09-01 01:42:42 +0200

    gathering user input data & handling form submission

--- student/src/components/ProfileInfo/AddStudent.js
+++ student/src/components/ProfileInfo/AddStudent.js
@@ -1,39 +1,69 @@
 import classes from "./AddStudent.module.css";
 import Card from "../UI/Card";
+import {useRef} from 'react';
 
 
 function AddStudent() {
+//using useref from react which acts as an object that will refernce
+//the current html element.
+
+//to extract that reference value entered in the input we use varaible.current.value
+
+
+    const usetitleRef=useRef();
+    const usenameRef=useRef();
+    const useageRef=useRef();
+    const useemaileRef=useRef();
+    const useaddressRef=useRef();
+    const useimageRef=useRef(); 
+
     function onclicksubmit(event){
     event.preventDefault();
-    }
+    const enteredtitle= usetitleRef.current.value;   
+    const enteredname= usenameRef.current.value;  
+    const enteredage= useageRef.current.value;  
+    const enteredemail= useemaileRef.current.value;
+    const enteredaddress= useaddressRef.current.value;  
+    const enteredimage= useimageRef.current.value;
     
+    const wholedata={
+        title:enteredtitle,
+        name:enteredname,
+        age:enteredage,
+        email:enteredemail,
+        address:enteredaddress,
+        image:enteredimage
+    };
+    console.log(wholedata);
+    }
+     
 
   return (
     <Card>
       <form className={classes.form} onSubmit={onclicksubmit}>
         <div className={classes.control}>
           <label htmlFor='title'>TITLE</label>
-          <input type='text' required id='title' placeholder='Enter Title'></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='Name'>NAME</label>
-          <input type='text' required id='Name' placeholder='Enter Name'></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='Age'>AGE</label>
-          <input type='text' required id='Age'placeholder='Enter Age'></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='email'>Email</label>
-          <input type='text' required id='email' placeholder='Enter Email'></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' placeholder='Enter Address'></input>
+          <input type='text' required id='title' placeholder='Enter Title' ref={usetitleRef}></input>
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Image</label>
-          <input type='url' required id='image' placeholder='Attach Link'></input>
+          <input type='url' required id='image' placeholder='Attach Link' ref={useimageRef}></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='Name'>NAME</label>
+          <input type='text' required id='Name' placeholder='Enter Name' ref={usenameRef}></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='Age'>AGE</label>
+          <input type='text' required id='Age'placeholder='Enter Age' ref={useageRef}></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='email'>Email</label>
+          <input type='text' required id='email' placeholder='Enter Email' ref={useemaileRef}></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='address'>Address</label>
+          <input type='text' required id='address' placeholder='Enter Address' ref={useaddressRef}></input>
         </div>
        <div className={classes.actions}>
         <button>ADD</button>
