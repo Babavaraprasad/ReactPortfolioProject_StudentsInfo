6fa4c81bd6816aefe283d058e81654597acebeac
Babavaraprasad <bapi@itu.dk>
2022-09-01 03:05:33 +0200

    using firebase service as a Realtime database to send & fetch API Requests

--- student/src/Pages/AddNew.js
+++ student/src/Pages/AddNew.js
@@ -2,11 +2,42 @@ import AddStudent from '../components/ProfileInfo/AddStudent';
 
 function AddNew()
 {
+   function addstudentdatahandler(wholedata){
+   fetch('https://react-studentsbiodata-default-rtdb.firebaseio.com/biodata.json',
+   {
+    method:'POST',
+    body:JSON.stringify(wholedata),
+    headers:
+    {'Content-Type':'application/json'
+    }
+   });
+   }
+
     return (
     <section>
         <h1>Add new student</h1>
-        <AddStudent />
+        <AddStudent something={addstudentdatahandler}/>
         </section>
     )
 };
 export default AddNew;
+/*here prop something will be sent to Addstudent component where data is being binded in the form of
+json and we are calling function named 'addstudentdatahandler'
+
+here the above function will be sending the http request using inbulit javascript function 'fetch'
+we can also use axios package in the react library to send http requests.
+
+for instance, above for the fetch function we use the firebase real time database 
+url and we can also add some extra url which represents folder or table in database.
+ex:https://react-studentsbiodata-default-rtdb.firebaseio.com/studentsbiodata.json'
+
+note:at the end we need to add .json extension which is required for firebase
+
+to insert the data we use POST request,to do it we add second argument in fetch which is an object
+which configures the fetch function call and we define the properties inside the object.
+
+properties to be defined inside second argument object fetch  are:
+1)methods/HTTP verbs(GET,POST,PUT,DELETE)
+2)Body -will be in json format & stringify converts arrays,values,expressions,strings to json data
+3)
+*/
--- student/src/components/ProfileInfo/AddStudent.js
+++ student/src/components/ProfileInfo/AddStudent.js
@@ -3,7 +3,7 @@ import Card from "../UI/Card";
 import {useRef} from 'react';
 
 
-function AddStudent() {
+function AddStudent(props) {
 //using useref from react which acts as an object that will refernce
 //the current html element.
 
@@ -34,7 +34,9 @@ function AddStudent() {
         address:enteredaddress,
         image:enteredimage
     };
-    console.log(wholedata);
+    //console.log(wholedata);
+
+   props.something(wholedata);
     }
      
 
@@ -74,3 +76,20 @@ function AddStudent() {
 }
 
 export default AddStudent;
+
+
+/*after clicking on formsubmit, all the user entered input data is gathered using "useref" objects provided by react library and we store the current input values 
+refernced in a variable and assign this data to different variabes in the form of a json format so we can send the json data to backend API which connects to database
+to store the records and performs CRUD operations in database.backend development languages might be java,c#,node,js and a open source database like postgres sql.
+
+Here what we simply do is that we make use of an open source google platform service "Firebase" which send the requests and handles the API calls,store and retrieve data
+similar to using the backend languages.
+
+
+how does it work?
+we send the data to firebase api and we use an request which directly interacts with database
+ where do we add http request?
+we can add in this file where we write the function for onsubmit() or we can also do it in the file where 
+current AddStudent.js is called that is Addnew.js 
+To do that im passing a prop from addnew.js page and in this jsx file im binding the wholedata.
+*/
